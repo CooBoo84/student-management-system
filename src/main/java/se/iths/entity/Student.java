f9a25dcd478235c6217f51510a375c52ab59b88b
CooBoo84 <kennie_svensson@hotmail.com>
2021-11-15 13:54:45 +0100

    feat(student): add annotation to class

--- src/main/java/se/iths/entity/Student.java
+++ src/main/java/se/iths/entity/Student.java
@@ -1,12 +1,33 @@
 package se.iths.entity;
 
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.Digits;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+@Entity
 public class Student {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
+    @NotEmpty
+    @Size(min = 2, message = "Firstname too short!")
     private String firstName;
+    @NotEmpty
+    @Size(min = 2, message = "Lastname too short!")
     private String lastName;
+    @NotEmpty
+    @Email(message = "Invalid email. Try again.")
     private String email;
+
+    @Digits(integer = 15, fraction = 0)
     private String phoneNumber;
 
 }
