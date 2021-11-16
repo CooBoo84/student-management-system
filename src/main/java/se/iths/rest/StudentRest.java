2f8049a334bc000046dc529b5a5b3dbdd7311501
CooBoo84 <kennie_svensson@hotmail.com>
2021-11-16 09:58:22 +0100

    feat(student-rest/-service): implement search by lastname

--- src/main/java/se/iths/rest/StudentRest.java
+++ src/main/java/se/iths/rest/StudentRest.java
@@ -17,40 +17,45 @@ public class StudentRest {
     @Inject
     StudentService studentService;
 
-    @Path("")
-    @POST
-    public Response createStudent(Student student){
-        studentService.createStudent(student);
-        return Response.ok(student).status(Response.Status.CREATED).build();
-    }
-
-    @Path("")
-    @PUT
-    public Response updateStudent(Student student){
-        studentService.updateStudent(student);
-        return Response.ok(student).build();
-    }
-
-    @Path("{id}")
     @GET
+    @Path("{id}")
     public Response getStudent(@PathParam("id") Long id){
         Student foundStudent = studentService.findStudentById(id);
         return Response.ok(foundStudent).build();
     }
 
-    @Path("")
     @GET
+    @Path("search")
+    public Response findByLastName(@QueryParam("lastName") String lastName) {
+        List<Student> students = studentService.getStudentByLastName(lastName);
+        return Response.ok(students).build();
+    }
+
+    @GET
+    @Path("")
     public Response getAllStudents(){
         List<Student> foundStudents = studentService.getAllStudents();
         return Response.ok(foundStudents).build();
     }
 
-    @Path("{id}")
+    @POST
+    @Path("")
+    public Response createStudent(Student student){
+        studentService.createStudent(student);
+        return Response.ok(student).status(Response.Status.CREATED).build();
+    }
+
+    @PUT
+    @Path("")
+    public Response updateStudent(Student student){
+        studentService.updateStudent(student);
+        return Response.ok(student).build();
+    }
+
     @DELETE
+    @Path("{id}")
     public Response deleteStudent(@PathParam("id") Long id){
         studentService.deleteStudent(id);
         return Response.ok().build();
     }
-
-
 }
--- src/main/java/se/iths/service/StudentService.java
+++ src/main/java/se/iths/service/StudentService.java
@@ -4,6 +4,7 @@ import se.iths.entity.Student;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
 import javax.transaction.Transactional;
 import java.util.List;
 
@@ -13,20 +14,27 @@ public class StudentService {
     @PersistenceContext
     EntityManager entityManager;
 
+
+    public Student findStudentById(Long id) {
+        return entityManager.find(Student.class, id);
+    }
+
+    public List<Student> getStudentByLastName(String lastName) {
+        TypedQuery<Student> query = entityManager.createQuery("SELECT s from Student s WHERE s.lastName = :lastName", Student.class);
+        query.setParameter("lastName", lastName);
+        return query.getResultList();
+    }
+
+    public List<Student> getAllStudents() {
+        return entityManager.createQuery("SELECT s from Student s", Student.class).getResultList();
+    }
+
     public void createStudent(Student student) {
         entityManager.persist(student);
     }
 
     public void updateStudent(Student student) {
         entityManager.merge(student);
-    }
-
-    public Student findStudentById(Long id) {
-        return entityManager.find(Student.class, id);
-    }
-
-    public List<Student> getAllStudents() {
-        return entityManager.createQuery("SELECT s from Student s", Student.class).getResultList();
     }
 
     public void deleteStudent(Long id) {
